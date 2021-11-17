5b4916a40910e419c8483024ba7d263ed8145727
Zachery Naylor <ender@thefluegges.com>
2021-11-17 14:57:28 -0500

    Added info to API

--- src/libs/API.java
+++ src/libs/API.java
@@ -14,23 +14,45 @@ public class API {
     public API() throws IOException {
         // Create new http server
         this._server = HttpServer.create(new InetSocketAddress("localhost", 8080), 0);
+
+        // All functions that add routes to the server should be added here.
         ping();
 
         this._server.start();
     }
 
+    /**
+     * This is an example route, a template for what they should look like.
+     * '_server' is the HttpServer object. This is what handles all incoming requests to the server,
+     * and is what you need to use to add a new route.
+     *
+     * The 'createContext' function on '_server' (_server.createContext) takes a path, and a function,
+     * and the function is called when a request is made to that path.
+     *
+     * Example, when this program is running if you go to "https://localhost:8080/ping" in your browser,
+     * this function will be called, and you'll see 'pong uwu' in the browser!
+     */
     private void ping() {
-        _server.createContext("/ping", new HttpHandler() {
-            @Override
-            public void handle(HttpExchange exchange) throws IOException {
-                String res = "PONG";
-                OutputStream body = exchange.getResponseBody();
-
-                exchange.sendResponseHeaders(200, res.length());
-                body.write(res.getBytes(StandardCharsets.UTF_8));
-                body.flush();
-                body.close();
-            }
+        _server.createContext("/ping", exchange -> {
+            writeResponse(exchange, "pong uwu");
         });
+    }
+
+    /**
+     * A short utility function to write a response to the client.
+     * @param exchange The HTTPExchange object.
+     * @param response The repsonse data you want to write.
+     */
+    private void writeResponse(HttpExchange exchange, String response) {
+        try {
+            OutputStream body = exchange.getResponseBody();
+
+            exchange.sendResponseHeaders(200, response.length());
+            body.write(response.getBytes(StandardCharsets.UTF_8));
+            body.flush();
+            body.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
